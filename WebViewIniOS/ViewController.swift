a33045525d1d721e77106127c19ea9f21dfe6d27
Noor Mohammed Anik <noormohammedanik@gmail.com>
2021-08-13 01:24:14 +0600

    update

--- WebViewIniOS/ViewController.swift
+++ WebViewIniOS/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
 
+    let webView = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+
+        //for loading url
+       guard let url = URL(string: "https://shorboraho.com")
+       else {
+        return
+       }
+        
+        webView.load(URLRequest(url: url))
+        
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+        
+    }
 
 }
 
